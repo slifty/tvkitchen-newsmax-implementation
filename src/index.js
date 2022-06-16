8d210aac6b63fc00a5797e128dcac47ccb0f8a32
Dan Schultz <slifty@gmail.com>
2022-06-16 09:52:32 -0400

    Set up the file topology
    This commit does too much at once, but basically here's what it covers:
    
    1) It uses dotenv for config settings, allowing us to specify AWS values
       as well as the kafka broker
    2) It adds our new appliances so that files will be generated and
       uploaded to AWS on a 30 minute cadence
    
    Issue #7 Support file generation and export

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
+import fs from 'fs'
+import dotenv from 'dotenv'
 import { Countertop } from '@tvkitchen/countertop'
 import {
 	WebVttHlsReceiverAppliance,
 	TextReducerAppliance,
+	FileRecorderAppliance,
+	SegmentGeneratorAppliance,
+	AwsUploaderAppliance,
 } from './appliances'
 import { CaptionSrtGeneratorAppliance } from '@tvkitchen/appliance-caption-srt-generator'
+import { INTERVALS } from '@tvkitchen/appliance-video-segment-generator'
 import { getTimestampFromNewsmaxWebVtt } from './utils'
+
+dotenv.config()
 
 const countertop = new Countertop({
 	kafkaSettings: {
-		brokers: ['kafka:9093'],
+		brokers: [process.env.KAFKA_BROKER],
 		connectionTimeout: 30000,
 	},
 })
+
+const origin = new Date();
+origin.setMinutes(0,0);
 
 countertop.addAppliance(
 	WebVttHlsReceiverAppliance,
@@ -21,10 +32,51 @@ countertop.addAppliance(
 	},
 )
 countertop.addAppliance(TextReducerAppliance)
+countertop.addAppliance(
+	SegmentGeneratorAppliance,
+	{
+		interval: INTERVALS.HOUR / 2,
+		startingAt: origin.toISOString(),
+		backfill: false,
+	},
+)
+countertop.addAppliance(
+	CaptionSrtGeneratorAppliance,
+	{ includeCounter: false },
+)
+countertop.addAppliance(
+	FileRecorderAppliance,
+	{
+		outDirectory: 'data',
+		appendSeparator: '\n',
+	},
+)
+countertop.addAppliance(
+	AwsUploaderAppliance,
+	{
+		deleteAfterUpload: false,
+		accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+		bucketName: process.env.AWS_BUCKET_NAME,
+		bucketConfiguration: {
+			LocationConstraint: process.env.AWS_BUCKET_REGION,
+		},
+	},
+)
 
-countertop.addAppliance(CaptionSrtGeneratorAppliance)
 countertop.on('data', (payload) => {
 	if (payload.type === 'TEXT.SRT') {
+	}
+	if (payload.type === 'FILE.OPENED') {
+		console.log('\nOPENED FILE')
+		process.stdout.write(payload.data)
+	}
+	if (payload.type === 'FILE.CLOSED') {
+		console.log('\nCLOSED FILE')
+		process.stdout.write(payload.data)
+	}
+	if (payload.type === 'FILE.UPLOADED') {
+		console.log('\nUPLOADED FILE')
 		process.stdout.write(payload.data)
 	}
 })
